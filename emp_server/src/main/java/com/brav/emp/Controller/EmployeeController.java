374ba6d9ace61f92959bdc805550f9737551aeb5
bravindan <danielbravin0@gmail.com>
2024-02-19 15:49:37 +0300

    Completecrud operations and endpoints

--- emp_server/src/main/java/com/brav/emp/Controller/EmployeeController.java
+++ emp_server/src/main/java/com/brav/emp/Controller/EmployeeController.java
@@ -2,10 +2,9 @@ package com.brav.emp.Controller;
 
 import com.brav.emp.models.Employee;
 import com.brav.emp.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,11 +20,35 @@ public class EmployeeController {
     public List getEmployees(){
         List<Employee> allEmployees = employeeService.getAllEmployees();
         return allEmployees;
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getEmployeeById(@PathVariable Long id) {
+            Employee employee = employeeService.getEmployeeById(id);
 
+        if (employee != null) {
+            return ResponseEntity.ok(employee);
+        } else {
+            return ResponseEntity.notFound().build();
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
     @PostMapping("/add")
     public Employee addEmployee(@RequestBody Employee employee){
         Employee newEmployee = employeeService.addNewEmployee(employee);
         return newEmployee;
+    }
+    @PutMapping("/edit/{id}")
+    Employee updateEmployee( @RequestBody Employee employee){
+        Employee updatedEmployee = employeeService.updateEmployee(employee);
+        return updatedEmployee;
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable Long id){
+        try{
+            employeeService.deleteEmployee(id);
+            return ResponseEntity.ok().build();
+        }catch (EmployeeService.RecordNotFoundException ex){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        }
     }
 }
--- emp_server/src/main/java/com/brav/emp/service/EmployeeService.java
+++ emp_server/src/main/java/com/brav/emp/service/EmployeeService.java
@@ -3,11 +3,12 @@ package com.brav.emp.service;
 import com.brav.emp.models.Employee;
 import com.brav.emp.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeService  {
@@ -19,18 +20,40 @@ public class EmployeeService  {
         return allEmployees;
     }
 
+    public Employee getEmployeeById(Long id){
+        return employeeRepository.findById(id).orElse(null);
+    }
+
     public Employee addNewEmployee(Employee newEmployee) {
         // Check if the newEmployee object is null
         if (newEmployee == null) {
             throw new IllegalArgumentException("Employee object cannot be null");
         }
-
         // Check if any required field is null or empty
         if (!isValidEmployee(newEmployee)) {
             throw new IllegalArgumentException("Employee data is incomplete");
         }
         return employeeRepository.save(newEmployee);
     }
+    public Employee updateEmployee(  Employee employee){
+        Long id = employee.getId();
+        return employeeRepository.findById(id).map(emp ->{
+            if (employee == null) {
+                throw new IllegalArgumentException("Employee object cannot be null");
+            }
+            // Check if any required field is null or empty
+            if (!isValidEmployee(employee)) {
+                throw new IllegalArgumentException("Employee data is incomplete");
+            }
+            return employeeRepository.save(employee);
+        } ).orElse(null);
+    }
+
+    public void deleteEmployee(Long id){
+        if(!employeeRepository.existsById(id)) throw new RecordNotFoundException("The record with id " + id + " not found");
+        employeeRepository.deleteById(id);
+    }
+
     private boolean isValidEmployee(Employee employee) {
         return !StringUtils.isEmpty(employee.getFirstName()) &&
                 !StringUtils.isEmpty(employee.getLastName()) &&
@@ -44,4 +67,12 @@ public class EmployeeService  {
                 !StringUtils.isEmpty(employee.getActive());
         // Add more conditions for other required fields if necessary
     }
+
+    public class RecordNotFoundException extends RuntimeException {
+        public RecordNotFoundException(String message) {
+            super(message);
+        }
+    }
+
+
 }
